2ce44ba91599b65dda551a604889c3e4aa10021d
Salvatore Giugno <73439256+salvatore9090@users.noreply.github.com>
2020-11-25 11:03:17 -0500

    Add files via upload
    Finally uploaded the Xcode files

---
+++ CarbonFood/CarbonFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarbonFood
+//
+//  Created by Salvatore Giugno on 2020-10-24.
+//  Copyright © 2020 Salvatore Giugno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
